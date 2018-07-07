d7a760e0de76a42ce2bd6a5a43204bcc3031c68d
Sk3ll <sk3llyar@gmail.com>
2018-07-07 19:59:48 +0300

    Add task 4_2

---
+++ Job4jTask4_2.java
@@ -0,0 +1,10 @@
+public class Job4jTask4_2 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+        int c = 5;
+        int d = 6;
+        int sum = a+b+c+d-10;
+        System.out.println(sum);
+    }
+}
